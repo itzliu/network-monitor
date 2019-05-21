276320d431e655abc4e4bb527862ec521b824f0f
Harry <harryliu1995@gmail.com>
2019-05-20 21:52:31 -0400

    update paths

--- scan.sh
+++ scan.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Detect hosts.
-nmap -sP 192.168.1.0/24 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" > /home/pi/router-monitor/detected-hosts
+nmap -sP 192.168.1.0/24 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" > /home/pi/network-monitor/detected-hosts
 
 # Compare detected hosts and known hosts.
-grep -Fxv -f /home/pi/router-monitor/known-hosts /home/pi/router-monitor/detected-hosts > /home/pi/router-monitor/unknown-hosts
+grep -Fxv -f /home/pi/network-monitor/known-hosts /home/pi/network-monitor/detected-hosts > /home/pi/network-monitor/unknown-hosts
 
 # Scan unknown hosts.
-if  [ -s /home/pi/router-monitor/unknown-hosts ]
+if  [ -s /home/pi/network-monitor/unknown-hosts ]
 then
-	nmap -O -iL /home/pi/router-monitor/unknown-hosts > /home/pi/router-monitor/unknown-hosts-scan
-	cat /home/pi/router-monitor/unknown-hosts-scan >> /home/pi/logs/unknown-hosts.log
+	nmap -O -iL /home/pi/network-monitor/unknown-hosts > /home/pi/network-monitor/unknown-hosts-scan
+	cat /home/pi/network-monitor/unknown-hosts-scan >> /home/pi/logs/unknown-hosts.log
 fi
