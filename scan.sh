4f35350f43f9433c56447431384dc718430d24f5
Harry <harryliu1995@gmail.com>
2019-05-22 00:03:31 -0400

    sudo nmap to show mac addresses and detect hosts using arp instead of nmap for faster run

--- scan.sh
+++ scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Detect hosts.
-nmap -sP 192.168.1.0/24 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" > $HOME/network-monitor/detected-hosts
+arp | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" > $HOME/network-monitor/detected-hosts
 
 # Compare detected hosts and known hosts.
 grep -Fxv -f $HOME/network-monitor/known-hosts $HOME/network-monitor/detected-hosts > $HOME/network-monitor/unknown-hosts
@@ -9,7 +9,7 @@ grep -Fxv -f $HOME/network-monitor/known-hosts $HOME/network-monitor/detected-ho
 # Scan unknown hosts.
 if  [ -s $HOME/network-monitor/unknown-hosts ]
 then
-	nmap -O -iL $HOME/network-monitor/unknown-hosts > $HOME/network-monitor/unknown-hosts-scan
+	sudo nmap -O -iL $HOME/network-monitor/unknown-hosts > $HOME/network-monitor/unknown-hosts-scan
 	cat $HOME/network-monitor/unknown-hosts-scan >> $HOME/logs/unknown-hosts.log
 else
 	timestamp() {
