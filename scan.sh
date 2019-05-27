97ee01c65d5b94c29e09d461f55c07f66fdd59db
Harry <harryliu1995@gmail.com>
2019-05-26 23:08:32 -0400

    change detection scan from arp to nmap ping scan

--- scan.sh
+++ scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Detect hosts.
-arp | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" > $HOME/network-monitor/detected-hosts
+nmap -sP 192.168.1.0/24 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" > $HOME/network-monitor/detected-hosts
 
 # Compare detected hosts and known hosts.
 grep -Fxv -f $HOME/network-monitor/known-hosts $HOME/network-monitor/detected-hosts > $HOME/network-monitor/unknown-hosts
